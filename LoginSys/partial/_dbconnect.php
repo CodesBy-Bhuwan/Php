142bdb20188b757ebd23f31a1dc0be3386120c7d
Bhuwan Bashyal <bhuwan.534610817loveswtkrokay@gmail.com>
2023-11-29 23:36:00 +0545

    2nd Phase

--- LoginSys/partial/_dbconnect.php
+++ LoginSys/partial/_dbconnect.php
@@ -1,13 +1,13 @@
 <?php 
-$server = "localhost";
+$dsn = "mysql:host=localhost; dbname=users564";
 $username = "root";
 $password = "";
 $database = "users";
-$conn = mysqli_connect($server, $username, $password, $database);
-if ($conn){
-    echo "success";
-}
-else{
+$conn = mysqli_connect($dsn, $username, $password, $database);
+if (!$conn){
+//     echo "success";
+// }
+// else{
     die("Error". mysqli_connect_error());
 }
 
